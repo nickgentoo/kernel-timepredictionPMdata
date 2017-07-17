66096a5fc2ffacad0904439eff18df542bc0c8b2
Nicolò Navarin <nnavarin@math.unipd.it>
2017-07-17 14:45:01 +0200

    new eps values

--- predict_stringkernel.py
+++ predict_stringkernel.py
@@ -38,7 +38,7 @@ kernel_test = kernel.string_kernel(X_test, X_train)
 print "Kernel computed"
 
 for C in  np.logspace(-12,10,num=23,base=10,dtype='float'):
-  for eps in [0.001,0.01,0.1,0.2,0.3,0.4,0.5,0.6]:
+  for eps in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.5,2.0,5.0,10.0,50.0,100.0]:
     svr=SVR(kernel='precomputed',C=C,epsilon=eps)
     svr.fit(kernel_train, y_train)
 
